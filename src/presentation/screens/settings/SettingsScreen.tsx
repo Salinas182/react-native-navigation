e7e37d9ff43affadac8aebcc48a1ed5096bf87c9
salinas182 <salinas.182@hotmail.com>
2024-05-23 21:10:20 +0200

    used popToTop in order to go back to the main page

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {StackActions, useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>SettingsScreen</Text>
+
+      <PrimaryButton
+        label="Back"
+        onPress={() => navigation.goBack()}
+      />
+      <PrimaryButton
+        label="Home"
+        onPress={() => navigation.dispatch(StackActions.popToTop())}
+      />
     </View>
   );
 };
